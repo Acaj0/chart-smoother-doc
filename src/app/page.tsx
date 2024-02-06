97b84c57963868d34a8009e9ef40332c54c6ac01
Murilo Luciano <murilo.luciano123@gmail.com>
2024-02-06 19:45:06 -0400

    Format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Head from "next/head";
 import Github from "@/components/icons/Github";
 import Npm from "@/components/icons/Npm";
@@ -10,14 +9,13 @@ export default function Home() {
       <Head>
         <title>test</title>
       </Head>{" "}
-    <div className="h-16 bg-gray-900 flex justify-between px-24 text-3xl items-center">
-      <h1 className="text-white">Chart-Smoother</h1>
-      <div className="w-32 flex justify-between ">
-      <Github/>
-      <Npm/>
+      <div className="h-16 bg-gray-900 flex justify-between px-24 text-3xl items-center">
+        <h1 className="text-white">Chart-Smoother</h1>
+        <div className="w-32 flex justify-between ">
+          <Github />
+          <Npm />
+        </div>
       </div>
-      
-    </div>
     </>
   );
 }
