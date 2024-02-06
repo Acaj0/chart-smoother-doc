e6898361611d43a974b451f60f154daa574bffb9
Murilo Luciano <murilo.luciano123@gmail.com>
2024-02-06 19:44:51 -0400

    Removing use client of icons components and fixing warning

--- src/components/icons/Npm.tsx
+++ src/components/icons/Npm.tsx
@@ -1,16 +1,26 @@
-"use client"
-export default function Npm (){
-    return(
-        <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => window.open("https://www.npmjs.com/package/chart-smoother", '_blank')}>
-        <g clip-path="url(#clip0_6_24)">
-        <path d="M0 36V0H36V36H0Z" fill="#C12127"/>
-<path d="M6.75 6.75H29.25V29.25H24.75V11.25H18V29.25H6.75V6.75Z" fill="white"/>
-</g>
-<defs>
-<clipPath id="clip0_6_24">
-<rect width="36" height="36" fill="white"/>
-</clipPath>
-</defs>
-</svg>
-    )
+export default function Npm() {
+  return (
+    <a href="https://www.npmjs.com/package/chart-smoother">
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_6_24)">
+          <path d="M0 36V0H36V36H0Z" fill="#C12127" />
+          <path
+            d="M6.75 6.75H29.25V29.25H24.75V11.25H18V29.25H6.75V6.75Z"
+            fill="white"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_6_24">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </a>
+  );
 }
