9e3033e8e5b694b3cb07ca2679f410f7e8ee0bad
Murilo Luciano <61948229+Murilo-Luciano@users.noreply.github.com>
2024-02-26 19:52:22 -0400

    UI change using shadcn/ui (#2)

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,12 +1,22 @@
 import Github from "./icons/Github";
 import Npm from "./icons/Npm";
+import { Separator } from "./ui/separator";
 
 export default function PageHeader() {
   return (
     <>
-      <div className="h-16 bg-blue flex justify-between px-24 text-3xl items-center">
-        <h1 className="text-white">Chart-Smoother</h1>
-        <div className="w-32 flex justify-between ">
+      <div className="h-16 bg-foreground flex justify-between px-24 items-center">
+        <a href="/">
+          <h1 className="text-secondary font-extrabold tracking-tight text-2xl">
+            chart-smoother
+          </h1>
+        </a>
+
+        <div className="w-72 flex justify-between items-center">
+          <a href="/playground" className="text-secondary text-lg font-bold">
+            Playground
+          </a>
+          <Separator className="h-10" orientation="vertical" />
           <Github />
           <Npm />
         </div>
